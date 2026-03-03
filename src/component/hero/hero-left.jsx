9e2c7a60f64c20fc956c0c5ef382f7806477a883
corebyte56 <fahim789m@gmail.com>
2026-03-04 04:59:31 +0600

    style: improve hero text responsive spacing

--- src/component/hero/hero-left.jsx
+++ src/component/hero/hero-left.jsx
@@ -4,7 +4,7 @@ import HeroLeftInput from './hero-left-input'
 
 const HeroLeft = () => {
   return (
-    <div>
+    <div className='mx-5 md:mx-0'>
         <HeroLeftText />
         <HeroLeftInput />
     </div>
