5a9a02ab763e39fb0e9125017cb9d8f0b54b6248
corebyte56 <fahim789m@gmail.com>
2026-03-15 06:37:18 +0600

    style: improve footer responsiveness for mobile and tablet

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterLeft from './footerLeft'
 import FooterRight from './footerRight'
 const Footer = () => {
   return (
-    <div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-200  border-t border-t-amber-500 mt-29  py-20 px-16'>
+    <div className='container mx-auto flex flex-col sm:flex-row justify-between gap-9 sm:gap-0 border-t border-t-amber-500 mt-29  py-20 px-16'>
       <FooterLeft />
       <FooterRight />
     </div>
